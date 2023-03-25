666d3d691667635d8c24eddcca864e747216d06a
dharan-dhakshan97 <dharanidhakshan97@gmail.com>
2023-03-25 20:50:16 +0530

    add initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Home from "./components/Home";
+import About from "./components/About";
+import Update from "./Arrays/Update";
 import './App.css';
+import Onedimensional from "./Arrays/Onedimensional";
 
 function App() {
   return (
     <>
       <Home/>
+      <About/>
+      <Update/>
+      <Onedimensional/>
     </>
   );
 }
